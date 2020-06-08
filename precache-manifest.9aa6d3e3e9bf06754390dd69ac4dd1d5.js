898982c225a5fd64f552059afe2ee474c71c4005
Joey Hotz <joeyhotz1@gmail.com>
2020-06-08 16:44:17 +1000

    pushing site

---
+++ precache-manifest.9aa6d3e3e9bf06754390dd69ac4dd1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a08c122b41b9a74e620114d86d6d378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae689ede99f2aa57537",
+    "url": "/static/css/main.f6661511.chunk.css"
+  },
+  {
+    "revision": "3d1320c4c5e94930bb8f",
+    "url": "/static/js/2.88cdbde5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88cdbde5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae689ede99f2aa57537",
+    "url": "/static/js/main.4f5655a8.chunk.js"
+  },
+  {
+    "revision": "d6da0f21712214605e18",
+    "url": "/static/js/runtime-main.e0a233ee.js"
+  }
+]);
